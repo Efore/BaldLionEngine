35077362249541686f9667c7e3bb3e50f770b3dd
Efore <eforetholagar@gmail.com>
2018-11-13 23:40:06 +0100

    Added premake, premake config file and bat

---
+++ premake5.lua
@@ -0,0 +1,103 @@
+workspace "BaldLionEngine"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug","Release","Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "BaldLionEngine"
+	location "BaldLionEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++14"
+		staticruntime "On"
+		systemversion "10.0.16299.0"
+
+		defines
+		{
+			"BL_BUILD_DLL",
+			"BL_PLATFORM_WINDOWS"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BL_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "BL_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"BaldLionEngine/vendor/spdlog/include",
+		"BaldLionEngine/src"
+	}
+
+	links
+	{
+		"BaldLionEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++14"
+		staticruntime "On"
+		systemversion "10.0.16299.0"
+
+		defines
+		{
+			"BL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BL_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "BL_DIST"
+		symbols "On"
