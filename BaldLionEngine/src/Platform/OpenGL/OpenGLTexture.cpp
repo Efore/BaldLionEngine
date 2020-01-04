8f4df30f18dcc8b837ffdb0242393ebeb0433db0
Iván León Santiago <eforetholagar@gmail.com>
2020-01-05 00:07:24 +0100

    added mipmapping

--- BaldLionEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ BaldLionEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -37,11 +37,12 @@ namespace BaldLion
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_rendererID);
 		glTextureStorage2D(m_rendererID, 1, internalFormat, m_width, m_height);
-
+		
 		glTextureParameteri(m_rendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTextureParameteri(m_rendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+		
 		glTextureSubImage2D(m_rendererID, 0, 0, 0, m_width, m_height, dataFormat, GL_UNSIGNED_BYTE, data);
+		glGenerateTextureMipmap(m_rendererID);
 
 		stbi_image_free(data);
 
